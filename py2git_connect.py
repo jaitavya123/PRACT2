0005591d9b56da1d7f37ddc423003fd04b49e22f
jaitavya <jaitavya123@gmail.com>
2024-07-16 12:38:48 +0530

    push 7

--- py2git_connect.py
+++ py2git_connect.py
@@ -1,6 +1,7 @@
 print("new LOC")
 print("1 LOC")
 print("2 LOC")
+<<<<<<< HEAD
 print("3-----------LOC")
 print("5-----------LOC")
 print("6-----------LOC")
@@ -8,3 +9,6 @@ print("7-----------LOC")
 print("8----")
 print("9............")
 print("10............")
+=======
+print("3-----------LOC")
+>>>>>>> parent of 2c317dc (push 3)
