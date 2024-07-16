0afa3e0ce7581c0391df3dd25a7dba251aa65355
jaitavya <jaitavya123@gmail.com>
2024-07-16 13:42:52 +0530

    after revert

--- py2git_connect.py
+++ py2git_connect.py
@@ -1,14 +1,9 @@
 print("new LOC")
 print("1 LOC")
 print("2 LOC")
-<<<<<<< HEAD
-print("3-----------LOC")
-print("5-----------LOC")
-print("6-----------LOC")
 print("7-----------LOC")
 print("8----")
 print("9............")
 print("10............")
-=======
 print("3-----------LOC")
->>>>>>> parent of 2c317dc (push 3)
+
