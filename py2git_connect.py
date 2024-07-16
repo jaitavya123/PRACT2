e515333f53a86f1e421f3c6f99be75a11a4f296b
jaitavya <jaitavya123@gmail.com>
2024-07-16 12:04:50 +0530

    new push

--- py2git_connect.py
+++ py2git_connect.py
@@ -1,11 +1,2 @@
-print("first line")
-print("second line")
-#<<<<<<< HEAD
-print("third line")
-print("4 line")
-print("5 line")
-print("6 line")
-print("7 line and roll back")
-#=======
-print("third line")
-#>>>>>>> parent of ad1505b (3 commit)
+print("new LOC")
+
