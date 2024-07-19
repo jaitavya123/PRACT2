ba66bf3f03aa653543d6c169fc05c2b0029d5f7e
jaitavya <jaitavya123@gmail.com>
2024-07-19 14:50:43 +0530

    call chrome via jenkins

--- py2git_connect.py
+++ py2git_connect.py
@@ -23,5 +23,6 @@ holdChrome = webdriver.ChromeOptions()
 holdChrome.add_experimental_option("detach", True)   #Restrict auto close of chrome
 e = webdriver.Chrome(holdChrome)
 e.get("https://keepr.inoidsolutions.in/")
+print(" calling chrome browser in jenkins")
 
 
